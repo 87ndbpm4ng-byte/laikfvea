9718e27c909280df0dd8ff74189b303d2e203a70
87ndbpm4ng-byte <87ndbpm4ng@privaterelay.appleid.com>
2026-07-22 13:30:05 +0300

    Restore technology page before components image

--- app/technology/page.tsx
+++ app/technology/page.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
-import { BottleExplorerSection } from "@/components/home/BottleExplorerSection";
 import { TechnologyHero } from "@/components/technology/TechnologyHero";
-import { TechnologyOverviewSection } from "@/components/technology/TechnologyOverviewSection";
 
 export const metadata: Metadata = {
   title: "Technology | Laikfvea",
@@ -10,11 +8,5 @@ export const metadata: Metadata = {
 };
 
 export default function TechnologyPage() {
-  return (
-    <>
-      <TechnologyHero />
-      <TechnologyOverviewSection />
-      <BottleExplorerSection />
-    </>
-  );
+  return <TechnologyHero />;
 }
