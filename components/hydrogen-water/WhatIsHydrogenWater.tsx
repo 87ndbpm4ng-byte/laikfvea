84f577336e4cc22d6c8e7d53ee4200107e076c92
87ndbpm4ng-byte <87ndbpm4ng@privaterelay.appleid.com>
2026-07-23 13:23:46 +0300

    Add hydrogen infographic below introduction

--- components/hydrogen-water/WhatIsHydrogenWater.tsx
+++ components/hydrogen-water/WhatIsHydrogenWater.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { hydrogenWaterContent } from "@/content/hydrogen-water";
 import { MotionSection } from "@/components/ui/MotionSection";
 import { SectionHeading } from "@/components/ui/SectionHeading";
@@ -7,14 +8,26 @@ export function WhatIsHydrogenWater() {
 
   return (
     <MotionSection className="px-6 py-28 sm:px-8 lg:px-10 lg:py-36">
-      <div className="mx-auto max-w-4xl">
-        <SectionHeading title={content.heading}>
-          {content.body.map((paragraph) => (
-            <p key={paragraph} className={paragraph === content.body[0] ? undefined : "mt-5"}>
-              {paragraph}
-            </p>
-          ))}
-        </SectionHeading>
+      <div className="mx-auto max-w-[1400px]">
+        <div className="max-w-4xl">
+          <SectionHeading title={content.heading}>
+            {content.body.map((paragraph) => (
+              <p key={paragraph} className={paragraph === content.body[0] ? undefined : "mt-5"}>
+                {paragraph}
+              </p>
+            ))}
+          </SectionHeading>
+        </div>
+        <div className="mx-auto mt-16">
+          <Image
+            src="/hydrogen/hydrogen-infographic.png"
+            alt="Educational infographic explaining hydrogen water and molecular hydrogen"
+            width={1774}
+            height={887}
+            sizes="(min-width: 1440px) 1400px, 100vw"
+            className="h-auto w-full object-contain"
+          />
+        </div>
       </div>
     </MotionSection>
   );
